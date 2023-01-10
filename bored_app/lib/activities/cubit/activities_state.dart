5a0881c03e541ca0281e0bebb13e9cfe3c412c0c
Craig Holbrook <craigholbrook33@gmail.com>
2023-01-09 21:22:42 -0500

    added tests for activities cubit

--- bored_app/lib/activities/cubit/activities_state.dart
+++ bored_app/lib/activities/cubit/activities_state.dart
@@ -2,15 +2,18 @@ part of 'activities_cubit.dart';
 
 enum ActivityRequestStatus { initial, loading, successful, failed }
 
-class ActivitiesState {
+class ActivitiesState extends Equatable {
   final ActivityRequestStatus status;
   final Activity? activity;
 
-  ActivitiesState({
+  const ActivitiesState({
     required this.status,
     this.activity,
   });
 
   @override
   String toString() => 'ActivitiesState(status: $status, activity: $activity)';
+
+  @override
+  List<Object?> get props => [status, activity];
 }
---
+++ bored_app/test/activities/cubit/activities_cubit_test.dart
@@ -0,0 +1,60 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:bored_app/activities/cubit/activities_cubit.dart';
+import 'package:bored_app/activities/models/activity.dart';
+import 'package:bored_app/activities/repositories/activities_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockActivitiesRepository extends Mock implements ActivitiesRepository {}
+
+class MockActivity extends Mock implements Activity {}
+
+void main() {
+  late ActivitiesRepository activitiesRepository;
+  late Activity activity;
+
+  group('ActivitiesCubit', () {
+    setUp(() {
+      activitiesRepository = MockActivitiesRepository();
+      activity = MockActivity();
+    });
+    test('initial state is correct', () {
+      final activitiesCubit = ActivitiesCubit(activitiesRepository: activitiesRepository);
+      expect(activitiesCubit.state, const ActivitiesState(status: ActivityRequestStatus.initial));
+    });
+
+    blocTest<ActivitiesCubit, ActivitiesState>(
+      'emits [loading, failed] when getActivity throws',
+      setUp: () {
+        when(() => activitiesRepository.getActivity()).thenThrow(Exception('oops'));
+      },
+      build: () => ActivitiesCubit(activitiesRepository: activitiesRepository),
+      act: (cubit) => cubit.getActivity(),
+      expect: () => <ActivitiesState>[
+        const ActivitiesState(status: ActivityRequestStatus.loading),
+        const ActivitiesState(status: ActivityRequestStatus.failed)
+      ],
+    );
+
+    blocTest(
+      'emits [loading, success with activity] when activitiesRepository.getActivity returns Activity ',
+      setUp: () {
+        when(() => activitiesRepository.getActivity()).thenAnswer((_) async => activity);
+      },
+      build: () => ActivitiesCubit(activitiesRepository: activitiesRepository),
+      act: ((cubit) => cubit.getActivity()),
+      expect: () => <ActivitiesState>[
+        const ActivitiesState(status: ActivityRequestStatus.loading),
+        ActivitiesState(status: ActivityRequestStatus.successful, activity: activity),
+      ],
+    );
+
+    blocTest<ActivitiesCubit, ActivitiesState>(
+      'reset emits initial state',
+      build: () => ActivitiesCubit(activitiesRepository: activitiesRepository),
+      seed: () => const ActivitiesState(status: ActivityRequestStatus.failed),
+      act: (cubit) => cubit.reset(),
+      expect: () => <ActivitiesState>[const ActivitiesState(status: ActivityRequestStatus.initial)],
+    );
+  });
+}
